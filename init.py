5f8264e45d37077ef1e4c46fd0314d05786131d7
raffs96 <raffs96@live.it>
2020-01-20 13:22:49 +0100

    Add parallel init

--- init.py
+++ init.py
@@ -1,6 +1,9 @@
 from random import *
 from elgamal import *
 from pwn import *
+from multiprocessing import Process
+
+
 import os
 # TODO increase number of bits
 
@@ -14,5 +17,17 @@ keysAlice=generate_keys(32,32)
 keysBob=generate_keys(32,32,keysAlice['publicKey'].p)
 proxyKey = generate_proxy_key(keysAlice['privateKey'].x,keysBob['privateKey'].x,keysAlice['publicKey'].p)
 
-print "python verifier.py " +  str(keysAlice['publicKey'].h) + " " + str(keysAlice['publicKey'].p) +" " +str(keysAlice['privateKey'].g) + " " + str(port) +" "+ str(nRound) + " "+ str(proxyKey) + " "+str(keysBob['publicKey'].h) 
-print "python proover.py " +  str(keysAlice['privateKey'].x) + " " +str(keysAlice['privateKey'].p) + " "+str(keysAlice['privateKey'].g)+ " "+ str(port) +" "+ str(nRound)
+def t1():
+    os.system("python verifier.py " +  str(keysAlice['publicKey'].h) + " " + str(keysAlice['publicKey'].p) +" " +str(keysAlice['privateKey'].g) + " " + str(port) +" "+ str(nRound) + " "+ str(proxyKey) + " "+str(keysBob['publicKey'].h) )
+
+def t2():
+    os.system( "python proover.py " +  str(keysAlice['privateKey'].x) + " " +str(keysAlice['privateKey'].p) + " "+str(keysAlice['privateKey'].g)+ " "+ str(port) +" "+ str(nRound))
+
+p = Process(target=t1, args=())
+p.start()
+sleep(1)
+p2 = Process(target=t2, args=())
+
+p2.start()
+p2.join()
+p.join()
