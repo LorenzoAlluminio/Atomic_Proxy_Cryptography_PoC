b12d1bee20e074da099c1b0111a7f3fa6309e8da
raffs96 <raffs96@live.it>
2020-01-20 20:03:20 +0100

    Minor fix

--- init.py
+++ init.py
@@ -15,10 +15,9 @@ port = 12345
 def generate_proxy_key(a,b,p):
     invb = inverse(b, p-1)
     return (a*invb)%(p-1)
-
+keysAlice=generate_keys(32,32)
+keysBob=generate_keys(32,32,keysAlice['publicKey'].p,keysAlice['publicKey'].g)
 if(useProxy):
-    keysAlice=generate_keys(32,32)
-    keysBob=generate_keys(32,32,keysAlice['publicKey'].p,keysAlice['publicKey'].g)
     proxyKey = generate_proxy_key(keysAlice['privateKey'].x,keysBob['privateKey'].x,keysAlice['publicKey'].p)
 
 def t1():
@@ -26,14 +25,14 @@ def t1():
     
         os.system( "python verifier.py " +  str(keysAlice['publicKey'].h) + " " + str(keysAlice['publicKey'].p) +" " +str(keysAlice['privateKey'].g) + " " + str(port) +" "+ str(nRound) + " "+ str(proxyKey) + " " + str(keysBob['publicKey'].h))
     else:
-        os.system("python verifier.py " +  str(keysAlice['publicKey'].h) + " " + str(keysAlice['publicKey'].p) +" " +str(keysAlice['privateKey'].g) + " " + str(port) +" "+ str(nRound) + " "+ str(proxyKey) + " "+str(keysBob['publicKey'].h) )
+        os.system("python verifier.py " +  str(keysAlice['publicKey'].h) + " " + str(keysAlice['publicKey'].p) +" " +str(keysAlice['privateKey'].g) + " " + str(port) +" "+ str(nRound) + " " )
 
 def t2():
     if(useProxy):
 
         os.system( "python prover.py " +  str(keysAlice['privateKey'].x) + " " +str(keysAlice['privateKey'].p) + " "+str(keysAlice['privateKey'].g)+ " "+ str(port) +" "+ str(nRound))
     else:
-        os.system( "python proover.py " +  str(keysAlice['privateKey'].x) + " " +str(keysAlice['privateKey'].p) + " "+str(keysAlice['privateKey'].g)+ " "+ str(port) +" "+ str(nRound))
+        os.system( "python prover.py " +  str(keysAlice['privateKey'].x) + " " +str(keysAlice['privateKey'].p) + " "+str(keysAlice['privateKey'].g)+ " "+ str(port) +" "+ str(nRound))
 
 p = Process(target=t1, args=())
 p.start()
