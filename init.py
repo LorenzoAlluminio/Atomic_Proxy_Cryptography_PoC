380181d4963f14a37253c9c064f5f42eb62df133
LorenzoAlluminio <alluminio.lorenzo@gmail.com>
2020-01-20 10:51:59 +0100

    fixed identification

--- init.py
+++ init.py
@@ -8,11 +8,15 @@ nRound = 10
 port = 12345
 def generate_proxy_key(a,b,p):
     invb = inverse(b, p-1)
-    return (a*invb)%(p)
+    return (a*invb)%(p-1)
 
 keysAlice=generate_keys(32,32)
-keysBob=generate_keys(32,32,keysAlice['publicKey'].p)
+keysBob=generate_keys(32,32,keysAlice['publicKey'].p,keysAlice['publicKey'].g)
+print keysAlice['privateKey'].g
+print keysBob['privateKey'].g
+print keysAlice['privateKey'].p
+print keysBob['privateKey'].p
 proxyKey = generate_proxy_key(keysAlice['privateKey'].x,keysBob['privateKey'].x,keysAlice['publicKey'].p)
 
-print "python verifier.py " +  str(keysAlice['publicKey'].h) + " " + str(keysAlice['publicKey'].p) +" " +str(keysAlice['privateKey'].g) + " " + str(port) +" "+ str(nRound) + " "+ str(proxyKey) + " "+str(keysBob['publicKey'].h)
+print "python verifier.py " +  str(keysAlice['publicKey'].h) + " " + str(keysAlice['publicKey'].p) +" " +str(keysAlice['privateKey'].g) + " " + str(port) +" "+ str(nRound) + " "+ str(proxyKey) + " " + str(keysBob['publicKey'].h)
 print "python prover.py " +  str(keysAlice['privateKey'].x) + " " +str(keysAlice['privateKey'].p) + " "+str(keysAlice['privateKey'].g)+ " "+ str(port) +" "+ str(nRound)
