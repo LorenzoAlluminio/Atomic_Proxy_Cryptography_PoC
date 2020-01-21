984e941fe33548ff4d5e7db05512d7795bd323d3
ODA <s262632@studenti.polito.it>
2020-01-21 17:04:23 +0100

    sign update

--- signature.py
+++ signature.py
@@ -11,49 +11,8 @@ l = 10
 def generate_proxy_key(a,b,p):
     invb = inverse(b, p-1)
     return (a*invb)%(p-1)
-'''
-def sign(m,keys,proxyKey=None):
-    a = keys['privateKey'].x
-    p=keys['publicKey'].p
-    g=keys['publicKey'].g
-    h = sha256()
-    k = []
-    s1 = []
-    for i in range(0,l):
-        # k is take between 1 and p-1 beacuse is applied to g;
-        #which is a generator of group of mod p-1
-        k.append(randint(1,p-1))
-        s1.append(modexp(g,k[i],p))
-        h.update(str(s1[i]))
-    hash = h.digest()
-    print "s1 --> " + str(s1)
-    print "generated hash --> " + hash
-    bits = []
-    for i in range(0,l):
-        bits.append(unpack("<B",hash[i])[0]%2)
 
-    print "extracted bits --> " + str(bits)
 
-    s2 = []
-    if(proxyKey != None):
-        print "using proxy key: " + str(proxyKey) + " to rencode message " + str(m)
-    for i in range(0,l):
-        #TODO potential timing attack
-        inva = inverse(a, p-1)
-        if bits[i] == 1:
-            temp = (k[i]-m)*inva
-            if(proxyKey != None):
-                temp = (temp * proxyKey)
-            s2.append(temp)
-        else:
-            temp = k[i]*inva
-            if(proxyKey != None):
-                temp = (temp * proxyKey)
-            s2.append(temp)
-
-    print "s2 --> " + str(s2)
-    return [s1,s2]
-'''
 def sign(m,keys,proxyKey=None):
     a = keys['privateKey'].x
     p=keys['publicKey'].p
@@ -120,18 +79,25 @@ def verify(m,signature,pk):
 
     return True
 
+#Data shared among different rounds
 keysAlice=generate_keys(32,32)
-# we need to pass Alice's p and g because they have to be shared in orther to create a valid proxy key
-keysBob=generate_keys(32,32,keysAlice['publicKey'].p,keysAlice['publicKey'].g)
-m = 1001
-print colored("Encripting the message " + str(m) +" with Alice private key", "green")
-signature = sign(m,keysAlice)
-print colored("Verify with Alice publickKey: ","green") + str(verify(m,signature,keysAlice['publicKey']))
-print colored("Verify with Bob publickKey: ","green")+ str(verify(m,signature,keysBob['publicKey']))
-
+keysBob=generate_keys(32,32,keysAlice['publicKey'].p,keysAlice['publicKey'].g) # we need to pass Alice's p and g because they have to be shared in orther to create a valid proxy key
+m= int(sys.argv[1])
 proxyKey = generate_proxy_key(keysAlice['privateKey'].x,keysBob['privateKey'].x,keysAlice['publicKey'].p)
-m = 1001
-print "Encripting the message " + str(m) + " with Alice private key and then applying the proxy key"
+
+#Signing with Alice key
+print colored("Signing the message " + str(m) +" with Alice private key", "green")
+signature = sign(m,keysAlice)
+print colored("Verify with Alice publickKey: ","green")
+print colored ("Result: ", "red") + str(verify(m,signature,keysAlice['publicKey']))
+print colored("Verify with Bob publickKey: ","green")
+print colored("Result: ","red") + str(verify(m,signature,keysBob['publicKey']))
+
+
+#Signing with alice key and then applying proxy key
+print colored("Signing the message " + str(m) + " with Alice private key and then applying the proxy key", "green")
 signature = sign(m,keysAlice,proxyKey)
-print colored("Verify with Alice publickKey: ","green") + str(verify(m,signature,keysAlice['publicKey']))
-print colored("Verify with Bob publickKey: ","green")+ str(verify(m,signature,keysBob['publicKey']))
+print colored("Verify with Alice publickKey: ","green")
+print colored ("Result: ", "red") + str(verify(m,signature,keysAlice['publicKey']))
+print colored("Verify with Bob publickKey: ","green")
+print colored("Result: ","red") + str(verify(m,signature,keysBob['publicKey']))
