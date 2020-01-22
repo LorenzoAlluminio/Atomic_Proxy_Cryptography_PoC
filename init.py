0c951d4824b019c9cc53854e65b3f7213a8d0a52
LorenzoAlluminio <alluminio.lorenzo@gmail.com>
2020-01-22 12:15:38 +0100

    refactored init

--- init.py
+++ init.py
@@ -2,37 +2,38 @@ from random import *
 from elgamal import *
 from pwn import *
 from multiprocessing import Process
-
-
 import os
 import sys
-# TODO increase number of bits
-
-useProxy = int(sys.argv[1])
-
-nRound = 5
-port = 12345
-def generate_proxy_key(a,b,p):
-    invb = inverse(b, p-1)
-    return (a*invb)%(p-1)
-keysAlice=generate_keys(32,32)
-keysBob=generate_keys(32,32,keysAlice['publicKey'].p,keysAlice['publicKey'].g)
-if(useProxy):
-    proxyKey = generate_proxy_key(keysAlice['privateKey'].x,keysBob['privateKey'].x,keysAlice['publicKey'].p)
 
 def t1():
     if(useProxy):
-
-        os.system( "python verifier.py " +  str(keysAlice['publicKey'].h) + " " + str(keysAlice['publicKey'].p) +" " +str(keysAlice['privateKey'].g) + " " + str(port) +" "+ str(nRound) + " " + str(keysBob['publicKey'].h))
+        os.system("python verifier.py " +  str(keysAlice['publicKey'].h) + " " + str(keysAlice['publicKey'].p) +" " +str(keysAlice['privateKey'].g) + " " + str(port) +" "+ str(nRound) + " " + str(keysBob['publicKey'].h))
     else:
         os.system("python verifier.py " +  str(keysAlice['publicKey'].h) + " " + str(keysAlice['publicKey'].p) +" " +str(keysAlice['privateKey'].g) + " " + str(port) +" "+ str(nRound) + " " + str(keysBob['publicKey'].h))
 
 def t2():
     if(useProxy):
-
         os.system( "python prover.py " +  str(keysAlice['privateKey'].x) + " " +str(keysAlice['privateKey'].p) + " "+str(keysAlice['privateKey'].g)+ " "+ str(port) +" "+ str(nRound)+ " "+ str(proxyKey) + " " + str(keysBob['publicKey'].h))
     else:
         os.system( "python prover.py " +  str(keysAlice['privateKey'].x) + " " +str(keysAlice['privateKey'].p) + " "+str(keysAlice['privateKey'].g)+ " "+ str(port) +" "+ str(nRound))
+
+
+# TODO increase number of bits
+
+useProxy = int(sys.argv[1])
+nRound = int(sys.argv[2])
+nRound += 1
+
+port = 12345
+def generate_proxy_key(a,b,p):
+    invb = inverse(b, p-1)
+    return (a*invb)%(p-1)
+
+keysAlice=generate_keys(32,32)
+keysBob=generate_keys(32,32,keysAlice['publicKey'].p,keysAlice['publicKey'].g)
+
+if(useProxy):
+    proxyKey = generate_proxy_key(keysAlice['privateKey'].x,keysBob['privateKey'].x,keysAlice['publicKey'].p)
 
 p = Process(target=t1, args=())
 p.start()
